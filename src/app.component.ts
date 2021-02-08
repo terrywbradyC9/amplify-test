b6f5abebc357ca2311abd7f956353d64e6622933
Terry Brady <terrywbrady@gmail.com>
2021-02-08 15:20:30 -0800

    test app

---
+++ src/app.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+export class AppComponent implements OnInit {
+  title = 'myamplifyapp';
+  async ngOnInit() {
+    // ... Place DataStore in here
+  }
+}
